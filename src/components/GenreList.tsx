c673354b80abaad70621324e8444693cf28f5a00
Mark Sapiandante <msapiandante.ccheetah@gmail.com>
2024-04-01 21:59:25 +0800

    filter games by genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,22 +1,29 @@
-import useGenres from '../hooks/useGenres'
-import { HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react'
-import getCroppedImageURL from '../services/image-url'
+import useGenres, { Genre } from "../hooks/useGenres";
+import { Button, HStack, Image, Link, List, ListItem, Spinner, Text } from "@chakra-ui/react";
+import getCroppedImageURL from "../services/image-url";
 
-const GenreList = () => {
-    const {data, isLoading, error} = useGenres()
-    if (error) return null
-    if (isLoading) return <Spinner />
-  return (
-    <List>
-        {data.map(genre => <ListItem paddingY='5px' key={genre.id}>
-          <HStack>
-            <Image width='32px' borderRadius={8} src={getCroppedImageURL(genre.image_background)} />
-            <Text fontSize='lg'>{genre.name}</Text>
-          </HStack>
-        </ListItem>)}
-
-    </List>
-  )
+interface Props {
+  onSelecGenre: (genre: Genre) => void;
 }
 
-export default GenreList
+const GenreList = ({ onSelecGenre }: Props) => {
+  const { data, isLoading, error } = useGenres();
+  if (error) return null;
+  if (isLoading) return <Spinner />;
+  return (
+    <List>
+      {data.map(genre => (
+        <ListItem paddingY="5px" key={genre.id}>
+          <HStack>
+            <Image width="32px" borderRadius={8} src={getCroppedImageURL(genre.image_background)} />
+            <Button onClick={() => onSelecGenre(genre)} fontSize="lg" variant="Link">
+              {genre.name}
+            </Button>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default GenreList;
