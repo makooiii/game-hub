a785c03a70d05762bd7917d8acc27050e60603b1
Mark Sapiandante <msapiandante.ccheetah@gmail.com>
2024-04-02 23:03:43 +0800

    handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,13 +1,11 @@
-
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp";
 // Crop images for optimized image
 
 const getCroppedImageURL = (url: string) => {
-    if(!url) return ''
-    const target = 'media/';
-    const index = url.indexOf(target) + target.length
-    return url.slice(0, index) + 'crop/600/400/' + url.slice(index)
+  if (!url) return noImage;
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+};
 
-}
-
-
-export default getCroppedImageURL
+export default getCroppedImageURL;
